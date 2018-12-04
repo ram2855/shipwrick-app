017e224c1e9e89082cd92c8b30c9a379b80d5105
MP Techno Solutions <41568630+devops-mptech@users.noreply.github.com>
2018-12-04 08:14:03 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -2,4 +2,4 @@ mv target/*.war target/app.war
 ssh -i /var/lib/jenkins/suresh.pem -o StrictHostKeyChecking=no ec2-user@34.222.138.180 rm -rf /opt/tomcat8/*.war
 scp -i /var/lib/jenkins/suresh.pem -o StrictHostKeyChecking=no /var/lib/jenkins/workspace/shipwrick/target/app.war ec2-user@34.222.138.180:/opt/tomcat8/webapps/
 ssh -i /var/lib/jenkins/suresh.pem -o StrictHostKeyChecking=no ec2-user@34.222.138.180 /opt/tomcat8/bin/shutdown.sh
-ssh -i /var/lib/jenkins/suresh.pem -o StrictHostKeyChecking=no ec2-user@34.222.138.180 /opt/tomcat8/bin/startup.sh
+#ssh -i /var/lib/jenkins/suresh.pem -o StrictHostKeyChecking=no ec2-user@34.222.138.180 /opt/tomcat8/bin/startup.sh
